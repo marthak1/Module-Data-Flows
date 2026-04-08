b895d159374196b4f2b6e8e85d1e0c67fd5dafc5
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-08 21:22:59 +0100

    defined destructing syntax and updated function parameter in exercise.js file

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,10 +3,10 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
-
+let {name, age, favouriteFood} = personOne
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself(aboutMe){
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
