f87e7eb9683092ab2225fa50470d1183e2bd0b6e
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-10 18:26:11 +0100

    format order function code

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -7,21 +7,21 @@ let order = [
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
 function orders(orderItems) {
-    const header = `${"QTY".padEnd(5)}${"ITEM".padEnd(20)}${"TOTAL (£)".padStart(10)}`;
-    console.log(header);
+  const header = `${"QTY".padEnd(5)}${"ITEM".padEnd(20)}${"TOTAL (£)".padStart(
+    10
+  )}`;
+  console.log(header);
   orderItems.forEach(({ quantity, itemName, unitPricePence }) => {
-     const totalPounds = ((quantity * unitPricePence) / 100).toFixed(2);
-       const row = `${String(quantity).padEnd(5)}${itemName.padEnd(20)}${totalPounds.padStart(10)}`;
-       console.log(row);
-    
-});
-         const grandTotal = orderItems.reduce(
-           (sum, { quantity, unitPricePence }) => {
-             return sum + quantity * unitPricePence;
-           },
-           0
-         );
-         console.log(`Total: £${(grandTotal/100).toFixed(2)}`);
+    const totalPounds = ((quantity * unitPricePence) / 100).toFixed(2);
+    const row = `${String(quantity).padEnd(5)}${itemName.padEnd(
+      20
+    )}${totalPounds.padStart(10)}`;
+    console.log(row);
+  });
+  const grandTotal = orderItems.reduce((sum, { quantity, unitPricePence }) => {
+    return sum + quantity * unitPricePence;
+  }, 0);
+  console.log(`Total: £${(grandTotal / 100).toFixed(2)}`);
 }
 
 orders(order);
