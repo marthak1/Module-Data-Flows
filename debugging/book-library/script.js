9fb77a91e8f2ff8478f497b30d537693bdc99d04
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-15 21:37:23 +0100

    removed unused comment

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -105,18 +105,3 @@ function render() {
     });
   }
 }
-// My website should be able to:
-
-// - View a list of books that I've read
-// - Add books to a list of books that I've read
-// - Including title, author, number of pages and if I've read it
-// - If any of the information is missing it shouldn't add the book and should show an alert
-// - Remove books from my list
-
-// ## Bugs to be fixed
-
-// 1. Website loads but doesn't show any books => fixed
-// 2. Error in console when you try to add a book => fixed
-// 3. It uses the title name as the author name => fixed
-// 4. Delete button is broken =>git 
-// 5. When I add a book that I say I've read - it saves the wrong answer => 
