11e500e7c6063c51b7ec3bfdec3880e682efff97
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-15 19:15:19 +0100

    update delete button

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-it 
 window.addEventListener("load", function (e) {
   populateStorage();
   render();
@@ -38,7 +37,7 @@ function submit() {
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    myLibrary.push(book);
     render();
   }
 }
@@ -89,7 +88,7 @@ function render() {
     });
 
     //add delete button to every row and render again
-    let delButton = document.createElement("button");
+    let delBut = document.createElement("button");
     delBut.id = i + 5;
     deleteCell.appendChild(delBut);
     delBut.className = "btn btn-warning";
@@ -101,3 +100,18 @@ function render() {
     });
   }
 }
+// My website should be able to:
+
+// - View a list of books that I've read
+// - Add books to a list of books that I've read
+// - Including title, author, number of pages and if I've read it
+// - If any of the information is missing it shouldn't add the book and should show an alert
+// - Remove books from my list
+
+// ## Bugs to be fixed
+
+// 1. Website loads but doesn't show any books
+// 2. Error in console when you try to add a book
+// 3. It uses the title name as the author name
+// 4. Delete button is broken
+// 5. When I add a book that I say I've read - it saves the wrong answer
